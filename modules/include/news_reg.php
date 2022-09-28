a63ad188c79e10b78b0c248e328380d746964694
Joel SV <joelpfaeskillsup@gmail.com>
2022-09-28 11:49:17 +0100

    comento brevemente el funcionamiento de la expresión regular para validar el número de teléfono

--- modules/include/news_reg.php
+++ modules/include/news_reg.php
@@ -49,7 +49,17 @@ function validar_nombre($name) {
 }
 
 //Función para validar el campo movil (\+34|0034|34)?[ -]*(6|7)[ -]*([0-9][ -]*){8}
+/*La primera parte (\+34|0034|34)? nos indica que el número que vamos a validar puede empezar por +34, 0034 o 34. Este es el código de país,
+el 34 pertenece a España, si quieres validar un móvil que no sea de España, lo único que tienes que hacer es cambiar ese código por el del país que quieras.
 
+La segunda parte [ -]* nos dice que detrás del código del país pueden ir espacios en blanco, guiones o puede que no haya ningún tipo de separación entre 
+los números, es decir, que el teléfono esté escrito seguido.
+
+La tercera parte (6|7)[ -]* nos está indicando que el primer número por el que empiece el número de teléfono (sin contar el código de país), tiene que ser 
+6 o 7 y que después de este primer número, al igual que en la segunda parte, puede ir un espacio en blanco, un guión o nada.
+
+Por último ([0-9][ -]*){8} nos está diciendo que tienen que ir 8 caracteres y que estos tienen que estar entre 0 y 9, ambos incluidos. Como en los casos 
+anteriores, detrás de cada uno de estos caracteres puede ir un espacio en blanco, un guión o nada. */
 function validar_movil($phone){
     if (!preg_match("(\+34|0034|34)?[ -]*(6|7)[ -]*([0-9][ -]*){8}",$phone)) {
         return false;
