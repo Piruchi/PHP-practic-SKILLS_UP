4d988828cb0b7331186e94a307e6075bb047e1ef
Joel SV <joelpfaeskillsup@gmail.com>
2022-09-29 11:51:09 +0100

    Actualización del código. He cambiado una de las variables y he empezado a verificar que los datos que me llegan tienen el formato que yo pido

--- modules/include/news_reg.php
+++ modules/include/news_reg.php
@@ -3,40 +3,43 @@
 
     $name = $email = $phone = $address = $city = $communities = $Zcode= $Newsletter= $NewsletterFormat = $othert="";
 
+    function limpiarDatos($data) {    //Esta función corrige errores previos que pueda haber puesto el usuario
+        $data = trim($data);    //Limpia los espacios tanto detrás como delante del string
+        $data = stripslashes($data);    //
+        $data = htmlspecialchars($data);    //Limpia caracteres especiales
+        return $data;
+    }
+
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        print_r ($_POST);
 
         if(!empty($_POST["name"]) || !empty ($_POST["email"]) || !empty($_POST["phone"])){
-        $name = $_POST["name"];
-        $email = $_POST["email"];
-        $phone = $_POST["phone"];
-        $address = $_POST["address"];
-        $city = $_POST["city"];
-        $communities = $_POST["communities"];
-        $Zcode = $_POST["Zcode"];
-        $Newsletter = $_POST["Newsletter"];
-        $NewsletterFormat = $_POST["Newsletter_format"];
-        $othert = $_POST["othert"];
+        $name = limpiarDatos($_POST["name"]);
+        $email = limpiarDatos($_POST["email"]);
+        $phone = limpiarDatos($_POST["phone"]);
+        $address = limpiarDatos($_POST["address"]);
+        $city = limpiarDatos($_POST["city"]);
+        $communities = limpiarDatos($_POST["communities"]);
+        $Zcode = limpiarDatos($_POST["Zcode"]);
+        $Newsletter = limpiarDatos($_POST["Newsletter[]"]);
+        $NewsletterFormat = limpiarDatos($_POST["Newsletter_format"]);
+        $othert = limpiarDatos($_POST["othert"]);
         }
         
         
-        echo "$name<br>";
+        /*echo "$name<br>";
         echo "$email<br>";
         echo "$phone<br>";
         echo "$address<br>";
         echo "$city<br>";
         echo "$communities<br>";
         echo "$NewsletterFormat<br>";
+        echo "$othert<br>";*/
+
+
   }
 
-  function limpiarDatos($data) {    //Esta función corrige errores previos que pueda haber puesto el usuario
-    $data = trim($data);    //Limpia los espacios tanto detrás como delante del string
-    $data = stripslashes($data);    //
-    $data = htmlspecialchars($data);    //Limpia caracteres especiales
-    return $data;
-}
-
-
-//Nombre, email y número de teléfono
+  //Nombre, email y número de teléfono
 
 //Función para validar que el campo nombre no esté vacío y que además cumpla las condiciones que queremos
 function validar_nombre($name) {
@@ -60,6 +63,7 @@ La tercera parte (6|7)[ -]* nos está indicando que el primer número por el que
 
 Por último ([0-9][ -]*){8} nos está diciendo que tienen que ir 8 caracteres y que estos tienen que estar entre 0 y 9, ambos incluidos. Como en los casos 
 anteriores, detrás de cada uno de estos caracteres puede ir un espacio en blanco, un guión o nada. */
+
 function validar_movil($phone){
     if (!preg_match("(\+34|0034|34)?[ -]*(6|7)[ -]*([0-9][ -]*){8}",$phone)) {
         return false;
@@ -68,4 +72,33 @@ function validar_movil($phone){
         return true;
     }
 }
+
+function validateEmail($email) {
+    if(filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        return true;
+        //echo "{$email}: A valid email"."<br>";
+    }
+    else {
+        return false;
+        //echo "{$email}: Not a valid email"."<br>";
+    }
+}
+
+  if(validar_nombre($name)){
+    echo "El nombre introducido es correcto: $name<br>";
+  } else {
+    echo "El nombre introducido es incorrecto: $name<br>";
+  }
+
+
+  //Hay que terminar esto, es simplemente validar y mostrar
+  if(validar_movil($phone)){
+    echo "El nombre introducido es correcto: $name<br>";
+  } else {
+    echo "El nombre introducido es incorrecto: $name<br>";
+  }
+
+
+
+
 ?>
