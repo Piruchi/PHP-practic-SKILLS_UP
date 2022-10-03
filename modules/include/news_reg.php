a46479139c13d88a7a5a7a835b78d56c0123f1a1
Joel SV <joelpfaeskillsup@gmail.com>
2022-10-03 09:39:34 +0100

    Comprobar cual de los datos obligatorios es erróneo y reestructuración del código (aún sin terminar). TENGO ALGO MAL EN LA IDENTACIONES, YA QUE AL CERRAR CIERTOS IF ME LO HACE DE FORMA EXTRAÑA, ADEMÁS ME FALTA PASAR A LA PUNTA DEL ABAJO EL ELSE QUE MANDA LOS MENSAJES DE ERROR.

--- modules/include/news_reg.php
+++ modules/include/news_reg.php
@@ -13,7 +13,7 @@
 
 
     //FUNCIONES DE VALIDACIÓN DE CAMPOS OBLIGATORIOS QUE USARÉ MÁS ADELANTE
-    
+
     //Función para validar que el campo nombre no esté vacío y que además cumpla las condiciones que queremos
 function validar_nombre($name) {
   if (!preg_match("/^[a-zA-Z-' ]*$/",$name)) {
@@ -51,17 +51,16 @@ function validateEmail($email) {
      */
 
     //OBLIGATORIOS
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
         print_r ($_POST);
 
-        if(!empty($_POST["name"]) || !empty ($_POST["email"]) || !empty($_POST["phone"])){
-        $name = limpiarDatos($_POST["name"]);
-        $email = limpiarDatos($_POST["email"]);
-        $phone = limpiarDatos($_POST["phone"]);
+    if(!empty($_POST["name"]) || !empty ($_POST["email"]) || !empty($_POST["phone"])){
+      $name = limpiarDatos($_POST["name"]);
+      $email = limpiarDatos($_POST["email"]);
+      $phone = limpiarDatos($_POST["phone"]);
 
 
-
-          //FUNCIONA NAME
+  //FUNCIONA NAME
   if(validar_nombre($name)){
     echo "El nombre introducido es correcto: $name<br>";
   } else {
@@ -76,11 +75,23 @@ function validateEmail($email) {
     $phone_err = true;
   }
 
-  //COMPRUEBO EMAIL
+  //Funciona EMAIL
   if(validateEmail($email)){
     echo "El correo introducido es correcto: $email<br>";
   } else {
     $email_err = true;
+  }
+        /*===================================================================================================== */
+  if (validar_nombre($name) || validateEmail($email) || validar_movil($phone)){
+
+  }else{
+    if ($name_err == true){
+      echo "El nombre que ha introducido no se corresponde con el formato solicitado";
+    }elseif($email_err == true){
+      echo "El email que ha introducido no se corresponde con el formato solicitado";
+    }elseif($phone_err == true){
+      echo "El número de teléfono que ha introducido no se corresponde con el formato solicitado";
+    }
   }
         /*==================================================================================================== */
 
@@ -133,7 +144,7 @@ function validateEmail($email) {
           $othert = null;
          }
         /*====================================================================================00 */
-        }
+    }
         
         
   /*echo "$name<br>";
@@ -146,31 +157,9 @@ function validateEmail($email) {
   echo "$othert<br>";*/
 
 
-  }
-
-
-
-
-// ESTA EXPRESIÓN REGULAR POR ALGÚN MOTIVO NO ME FUNCIONA DE FORMA CORRECTA, LA HE CAMBIADO POR UNA MÁS SENCILLA
-
-//Función para validar el campo movil (\+34|0034|34)?[ -]*(6|7)[ -]*([0-9][ -]*){8}
-/*La primera parte (\+34|0034|34)? nos indica que el número que vamos a validar puede empezar por +34, 0034 o 34. Este es el código de país,
-el 34 pertenece a España, si quieres validar un móvil que no sea de España, lo único que tienes que hacer es cambiar ese código por el del país que quieras.
-
-La segunda parte [ -]* nos dice que detrás del código del país pueden ir espacios en blanco, guiones o puede que no haya ningún tipo de separación entre 
-los números, es decir, que el teléfono esté escrito seguido.
-
-La tercera parte (6|7)[ -]* nos está indicando que el primer número por el que empiece el número de teléfono (sin contar el código de país), tiene que ser 
-6 o 7 y que después de este primer número, al igual que en la segunda parte, puede ir un espacio en blanco, un guión o nada.
-
-Por último ([0-9][ -]*){8} nos está diciendo que tienen que ir 8 caracteres y que estos tienen que estar entre 0 y 9, ambos incluidos. Como en los casos 
-anteriores, detrás de cada uno de estos caracteres puede ir un espacio en blanco, un guión o nada. */
-
-
-
-
-
-
+} else{
+  echo "Método post no ha llegado";
+}
 
 
 ?>
